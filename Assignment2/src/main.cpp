b2c7335790299cc5f93199eaa8b2c3f9308116fd
amomorning <amomorning@gmail.com>
2020-03-12 23:27:11 +0800

    Add Assignment3

--- Assignment2/src/main.cpp
+++ Assignment2/src/main.cpp
@@ -32,8 +32,8 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float z
 {
     // TODO: Copy-paste your implementation from the previous assignment.
     Eigen::Matrix4f projection;
-    float f = zFar;
-    float n = zNear;
+    float f = -zFar;
+    float n = -zNear;
     float t = std::tan(eye_fov * MY_PI / 180.0 / 2.0) * std::fabs(n);
     float b = -t;
     float r = t * aspect_ratio;
