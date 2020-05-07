7c77b161f4184dc0987ea982e7875bb70db735d5
amomorning <amomorning@gmail.com>
2020-05-07 16:43:25 +0800

    add comment

--- Final/code/main.cpp
+++ Final/code/main.cpp
@@ -12,10 +12,11 @@ void calculateNormals(const Eigen::MatrixXd &V, const Eigen::Matrix3Xi &F) {
     Eigen::MatrixXd N;
     N.resize(V.rows(), V.cols()); 
 
-    for (int i = 0; i < F.cols(); i++) {
+    for (int i = 0; i < F.cols(); ++ i) {
  
         auto vid = F.col(i);
         for (int i = 0; i < 3; ++ i) {
+            // edge b ccw to a
             Eigen::Vector3d a = V.col(vid((i+1)%3)) - V.col(vid(i));
             Eigen::Vector3d b = V.col(vid((i+2)%3)) - V.col(vid(i));
 
@@ -25,6 +26,14 @@ void calculateNormals(const Eigen::MatrixXd &V, const Eigen::Matrix3Xi &F) {
             N.col(vid(i)) += fn * angle;
         }
     }
+
+    // don't forget to normalize
+    for (int i = 0; i < V.cols(); ++ i) {
+        N.col(i).normalize();
+    }
+
+    // check point 1603
+    std::cout << "v 1603 = " << N.col(1603) << std::endl;
     common::write_matrix_binary_to_file("../data/V", V);
     common::write_matrix_binary_to_file("../data/N", N);
 }
